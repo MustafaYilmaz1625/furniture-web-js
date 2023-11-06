8c6c21f0e347373da6ecaf41450ed5c0187e2d1c
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-11-06 23:54:08 +0300

    Add banner section and style

--- app/app.js
+++ app/app.js
@@ -7,3 +7,14 @@ mobileMenuButton.addEventListener("click",function () {
     mobileMenuButton.classList.toggle("bx-x")
     mobileMenu.classList.toggle("hidden")
 })
+
+const header= document.querySelector("header");
+window.addEventListener("scroll", function() {
+    if(this.window.scrollY > 100){
+        header.classList.toggle("sticky", window.scrollY > 0);
+        header.style.backgroundColor = "white";
+        header.classList.add("shadow");
+    }else {
+        header.classList.remove("shadow");
+    }
+})
